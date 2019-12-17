d667f5afec453dd7eb2c262d4a484cc2c41c8044
Max Pinheiro <maxpinheiro181@gmail.com>
2019-12-17 18:47:23 -0500

    ship rendering and moving

--- dataDefinitions.js
+++ dataDefinitions.js
@@ -1,1 +1,56 @@
 
+/* A Ship is a new Ship(Number)
+Interpretation: represents the ship, where:
+ - alive represents whether the ship is invisible (0), alive (1), or broken (2).
+*/
+class Ship {
+    constructor() {
+        this.alive = 0;
+        this.x = width / 2;
+        this.y = height / 2;
+        this.radius = 12;
+        this.rotation = 0;
+        this.rotationSpeed = 0;
+        this.velocity = 0;
+        this.acceleration = 0;
+
+        this.draw = function () {
+            if (this.alive === 1) {
+                // draw ship
+                push();
+                translate(this.x, this.y);
+                rotate(this.rotation);
+                noFill();
+                stroke(255);
+                triangle(-this.radius, this.radius, this.radius, this.radius, 0, -this.radius * 1.25);
+                pop();
+
+                // draw flame if accelerating
+                if (this.acceleration > 0) {
+                    push();
+                    translate(this.x, this.y);
+                    rotate(this.rotation);
+                    stroke(255);
+                    noFill();
+                    triangle(-this.radius * 0.5, this.radius, this.radius * 0.5, this.radius, 0, this.radius * 2);
+                    pop();
+                }
+            }
+        };
+
+        this.update = function () {
+            this.rotation += this.rotationSpeed;
+            this.x += this.velocity * cos(this.rotation - 90) * 0.5;
+            this.x = (this.x + width) % width;
+            this.y += this.velocity * sin(this.rotation - 90) * 0.5;
+            this.y = (this.y + height) % height;
+
+            this.velocity += this.acceleration;
+            this.velocity = min(this.velocity, 15);
+            if (this.velocity <= 0) {
+                this.acceleration = 0;
+                this.velocity = 0;
+            }
+        };
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,1 +1,64 @@
+var gameState, ship;
 
+function setup() {
+    createCanvas(700, 500);
+    angleMode(DEGREES);
+    gameState = -2;
+    textAlign(CENTER);
+    textFont('Futura');
+    ship = new Ship();
+}
+
+function draw() {
+    background(0);
+    // initial screen
+    if (gameState === -2) {
+        fill(255);
+        noStroke();
+        textSize(50);
+        text("ASTEROIDS", width / 2, height / 2);
+        textSize(15);
+        text("PRESS SPACE TO START", width / 2, height / 2 + 50)
+    } else if (gameState === 0) {
+        // draw ship
+        ship.draw();
+        // update ship
+        ship.update();
+
+    }
+}
+
+function keyPressed() {
+    // creating ship
+    if (key === ' ') {
+        if (gameState === -2) {
+            gameState = 0;
+        }
+        console.log("space");
+        if (ship.alive === 0) {
+            ship.alive = 1;
+        }
+    }
+
+    // rotating
+    if (keyCode === RIGHT_ARROW) {
+        ship.rotationSpeed = 3;
+    } else if (keyCode === LEFT_ARROW) {
+        ship.rotationSpeed = -3;
+    }
+
+    // moving
+    if (keyCode === UP_ARROW) {
+        ship.acceleration = 0.5;
+    }
+}
+
+function keyReleased() {
+    if (keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW) {
+        ship.rotationSpeed = 0;
+    }
+
+    if (keyCode === UP_ARROW) {
+        ship.acceleration = -0.07;
+    }
+}
