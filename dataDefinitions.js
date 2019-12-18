dc0b8c9070a0c4c69f86b671644f68815148a193
Max Pinheiro <maxpinheiro181@gmail.com>
2019-12-18 12:26:47 -0500

    added bullet class, rendering, and updating

--- dataDefinitions.js
+++ dataDefinitions.js
@@ -1,14 +1,21 @@
 
-/* A Ship is a new Ship(Number)
+/* A Ship is a new Ship(Number Number Number Number Number Number Number Number)
 Interpretation: represents the ship, where:
- - alive represents whether the ship is invisible (0), alive (1), or broken (2).
+ - x represents the ship's center (x-coordinate), 
+ - y represents the ship's center (y-coordinate), 
+ - radius represents the ship's radius, 
+ - alive represents whether the ship is invisible (0), alive (1), or broken (2), 
+ - rotation represents how much the ship is rotated from facing North (clockwise, degrees), 
+ - rotationSpeed represents how fast the ship is currently turning
+ - velocity represents how fast the ship is moving, and
+ - acceleration represents how fast the ship is accelerating.
 */
 class Ship {
     constructor() {
-        this.alive = 0;
         this.x = width / 2;
         this.y = height / 2;
         this.radius = 12;
+        this.alive = 0;
         this.rotation = 0;
         this.rotationSpeed = 0;
         this.velocity = 0;
@@ -50,6 +57,44 @@ class Ship {
             if (this.velocity <= 0) {
                 this.acceleration = 0;
                 this.velocity = 0;
+            }
+        };
+    }
+}
+
+/* A Bullet is a new Bullet (Number Number Boolean)
+Intepretation: represents a Bullet shot from the ship, where:
+ - x represents the bullet's center (x-coordinate), 
+ - y represents the bullet's center (y-coordinate), 
+ - direction represents the direction the bullet is facing, and
+ - alive represents whether the bullet is live (shot and on-screen) or not.
+*/
+
+class Bullet {
+    constructor(x, y, direction) {
+        this.x = x;
+        this.y = y;
+        this.radius = 3;
+        this.direction = direction - 90;
+        this.alive = true;
+
+        this.draw = function () {
+            if (this.alive) {
+                stroke(255);
+                push();
+                //strokeWeight(2);
+                translate(this.x, this.y);
+                line(- this.radius * cos(this.direction), -this.radius * sin(this.direction),
+                    this.radius * cos(this.direction), this.radius * sin(this.direction));
+                pop();
+            }
+        };
+
+        this.update = function () {
+            this.x += 8 * cos(this.direction);
+            this.y += 8 * sin(this.direction);
+            if (this.x - this.radius > width || this.x + width < 0 || this.y - this.radius > height || this.y + this.radius < 0) {
+                this.alive = false;
             }
         };
     }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var gameState, ship;
+var gameState, ship, bullets;
 
 function setup() {
     createCanvas(700, 500);
@@ -7,9 +7,11 @@ function setup() {
     textAlign(CENTER);
     textFont('Futura');
     ship = new Ship();
+    bullets = [];
 }
 
 function draw() {
+    //console.log(bullets.length);
     background(0);
     // initial screen
     if (gameState === -2) {
@@ -22,21 +24,36 @@ function draw() {
     } else if (gameState === 0) {
         // draw ship
         ship.draw();
+        for (var i = 0; i < bullets.length; i++) {
+            bullets[i].draw();
+        }
         // update ship
         ship.update();
-
+        for (var i = 0; i < bullets.length; i++) {
+            bullets[i].update();
+        }
+        // remove dead bullets
+        for (var i = 0; i < bullets.length; i++) {
+            if (!bullets[i].alive) {
+                bullets.splice(i, 1);
+            }
+        }
     }
 }
 
 function keyPressed() {
     // creating ship
     if (key === ' ') {
+        // if beginning of game --> create ship
         if (gameState === -2) {
             gameState = 0;
-        }
-        console.log("space");
-        if (ship.alive === 0) {
-            ship.alive = 1;
+            if (ship.alive === 0) {
+                ship.alive = 1;
+            }
+        } else if (gameState === 0) {
+            if (ship.alive === 1) {
+                bullets.push(new Bullet(ship.x + ship.radius * cos(ship.rotation - 90), ship.y + ship.radius * sin(ship.rotation - 90), ship.rotation));
+            }
         }
     }
 
