4c134fe9c04a5a3ce12608c1b27399530b0533a0
Max Pinheiro <maxpinheiro181@gmail.com>
2019-12-26 16:53:29 -0500

    dying animation, most of recovery animation and gameplay

--- dataDefinitions.js
+++ dataDefinitions.js
@@ -8,6 +8,7 @@ Interpretation: represents the ship, where:
  - alive represents whether the ship is invisible (0), alive (1), or broken (2), 
  - rotation represents how much the ship is rotated from facing North (clockwise, degrees), 
  - rotationSpeed represents how fast the ship is currently turning.
+ - dying represents if the ship is dying (> 10) or not (0)
 */
 class Ship {
     constructor() {
@@ -17,8 +18,11 @@ class Ship {
 
         this.radius = 12;
         this.alive = 0;
+        this.dying = 0;
         this.rotation = -90;
         this.rotationSpeed = 0;
+        this.score = 0;
+        this.recovering = 0;
 
         this.draw = function () {
             if (this.alive === 1) {
@@ -41,35 +45,74 @@ class Ship {
                     triangle(-this.radius * 0.5, this.radius, this.radius * 0.5, this.radius, 0, this.radius * 2);
                     pop();
                 }
+                
+                // draw shield if recovering
+                if (this.recovering > 0) {
+                    stroke(0, 145, 255);
+                    fill(255, 50);
+                    circle(this.pos.x, this.pos.y, this.radius * 3);
+                }
+            } else if (this.alive === 2 || this.dying > 0) { // break into pieces
+                push();
+                translate(this.pos.x, this.pos.y);
+                rotate(this.rotation + 90);
+                var offset = (10 - this.dying) * 2;
+                stroke(255);
+                noFill();
+                line(-this.radius - offset, this.radius, - offset, -this.radius * 1.25);
+                line(this.radius + offset, this.radius, offset, -this.radius * 1.25);
+                line(-this.radius, this.radius + offset, this.radius, this.radius + offset);
+                pop();
             }
         };
 
         this.update = function () {
-            this.rotation += this.rotationSpeed * 0.75;
+            if (this.recovering > 0) {
+                this.recovering = max(this.recovering - 0.05, 0);
+            }
+            if (this.dying === 0) {
+                this.rotation += this.rotationSpeed * 0.75;
 
-            // update position
-            this.pos.add(this.vel);
-            // wrap ship around edges
-            this.pos.set((this.pos.x + width + this.radius) % (width + this.radius),
-                (this.pos.y + height + this.radius) % (height + this.radius));
+                // update position
+                this.pos.add(this.vel);
+                // wrap ship around edges
+                this.pos.set((this.pos.x + width + this.radius) % (width + this.radius),
+                    (this.pos.y + height + this.radius) % (height + this.radius));
 
-            // update velocity
-            if (this.accelerating) { // if accelerating --> increase based on current direction
-                var a = createVector(0.15 * cos(ship.rotation), 0.15 * sin(ship.rotation));
-                this.vel.add(a);
-            } else { // gravity --> decelerate
-                this.vel.mult(0.97);
+                // update velocity
+                if (this.accelerating) { // if accelerating --> increase based on current direction
+                    var a = createVector(0.15 * cos(ship.rotation), 0.15 * sin(ship.rotation));
+                    this.vel.add(a);
+                } else { // gravity --> decelerate
+                    this.vel.mult(0.97);
+                }
+
+                // cap velocity
+                if (this.vel.mag() >= 15) {
+                    this.vel.setMag(15);
+                }
+            } else {
+                this.dying = max(this.dying - 0.06, 0);
+                if (this.dying === 0) {
+                    this.resetShip();
+                    this.recovering = 10;
+                }
             }
 
-            // cap velocity
-            if (this.vel.mag() >= 15) {
-                this.vel.setMag(15);
-            }
         };
 
         this.hits = function (asteroid) {
             var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
             return (d < this.radius + asteroid.radius);
+        };
+
+        this.resetShip = function () {
+            this.alive = 1;
+            this.pos = createVector(width / 2, height / 2);
+            this.vel = createVector(0, 0);
+            this.accelerating = false;
+            this.rotation = -90;
+            this.rotationSpeed = 0;
         };
     }
 }
@@ -117,17 +160,43 @@ class Bullet {
 
 class Asteroid {
     constructor(pos, radius) {
-        if (pos) {
-            this.pos = pos.copy();
-        } else {
-            this.pos = createVector(random(width), random(height));
-        }
-        this.vel = p5.Vector.random2D();
         if (radius) {
             this.radius = radius * 0.5;
         } else {
             this.radius = 50;
         }
+
+        // if given position (child of collision), create with random velocity
+        if (pos) {
+            this.pos = pos.copy();
+            this.vel = p5.Vector.random2D();
+        } else { // randomly generate position and appropriate velocity
+            switch (ceil(random(4))) {
+                case 1: // start on left side, move to the right 
+                    //this.pos = createVector(-this.radius, random(height));
+                    this.pos = createVector(0, random(height));
+                    this.vel = createVector(random(0, 1), random(-1, 1));
+                    break;
+                case 2: // start on right side, move to the left
+                    //this.pos = createVector(width + this.radius, random(height));
+                    this.pos = createVector(width, random(height));
+                    this.vel = createVector(random(-1, 0), random(-1, 1));
+                    break;
+                case 3: // start on top, move down
+                    //this.pos = createVector(random(width), -this.radius);
+                    this.pos = createVector(random(width), 0);
+                    this.vel = createVector(random(-1, 1), random(0, 1));
+                    break;
+                case 4: // start on bottom, move up
+                    //this.pos = createVector(random(width), height + this.radius);
+                    this.pos = createVector(random(width), height);
+                    this.vel = createVector(random(-1, 1), random(-1, 0));
+                    break;
+
+            }
+        }
+
+
         this.vertices = 10;
         this.offsets = [];
         // initialize offsets
--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
-var gameState, ship, bullets, asteroids;
+var gameState, ship, bullets, asteroids, time;
 
 function setup() {
     createCanvas(700, 500);
     angleMode(DEGREES);
     gameState = -2;
-    textAlign(CENTER);
     textFont('Futura');
     ship = new Ship();
     bullets = [];
@@ -12,6 +11,7 @@ function setup() {
     for (var i = 0; i < 15; i++) {
         asteroids.push(new Asteroid(createVector(random(width), random(height)), random(30, 100)));
     }
+    time = 0;
 }
 
 function draw() {
@@ -29,6 +29,7 @@ function draw() {
         fill(255);
         noStroke();
         textSize(50);
+        textAlign(CENTER);
         text("ASTEROIDS", width / 2, height / 2);
         textSize(15);
         text("PRESS SPACE TO START", width / 2, height / 2 + 50);
@@ -41,8 +42,22 @@ function draw() {
         for (var i = 0; i < asteroids.length; i++) {
             asteroids[i].draw();
         }
+        
+        textSize(25);
+        textAlign(LEFT);
+        fill(255, 50);
+        noStroke();
+        text(ship.score, 5, 25);
 
         // UPDATE
+        
+        // add asteroids
+        if (time % 200 === 0 && asteroids.length < 15) {
+            asteroids.push(new Asteroid());
+        }
+
+        time ++;
+
         ship.update();
         for (var i = 0; i < bullets.length; i++) {
             bullets[i].update();
@@ -62,11 +77,19 @@ function draw() {
         for (var i = 0; i < bullets.length; i++) {
             for (var j = asteroids.length - 1; j >= 0; j--) {
                 if (bullets[i].hits(asteroids[j])) {
-                    if (asteroids[j].radius > 15) {
+                    if (asteroids[j].radius === 50) { // big asteroid
+                        ship.score += 20;
+                    } else if (asteroids[j].radius === 25) { // medium asteroid
+                        ship.score += 50;
+                    } else if (asteroids[j].radius === 12.5) { // small asteroid
+                        ship.score += 100;
+                    } else { // mini asteroid
+                        ship.score += 150;
+                    }
+                    
+                    if (asteroids[j].radius > 12) {
                         var newAsteroids = asteroids[j].breakup();
                         asteroids = asteroids.concat(newAsteroids);
-                    } else {
-                        // increase score?
                     }
                     asteroids.splice(j, 1);
                     bullets.splice(i, 1);
@@ -76,7 +99,9 @@ function draw() {
         }
 
         for (var i = 0; i < asteroids.length; i++) {
-            if (ship.hits(asteroids[i])) {
+            if (ship.hits(asteroids[i]) && ship.alive === 1 && ship.recovering === 0) {
+                ship.dying = 10;
+                ship.alive = 2;
                 console.log("ooops!");
             }
         }
@@ -89,6 +114,7 @@ function keyPressed() {
         // if beginning of game --> create ship
         if (gameState === -2) {
             gameState = 0;
+            asteroids = [];
             if (ship.alive === 0) {
                 ship.alive = 1;
             }
